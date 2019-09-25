1f7261ee0609b78488be61d9743423069961365f
ggomaeng <sungwoopark95@gmail.com>
2019-09-26 01:32:02 +0900

    temporary error message handling

--- src/utils/errorMessage.js
+++ src/utils/errorMessage.js
@@ -1,5 +1,11 @@
 import { notification } from "antd";
 
+//temporary
+const errorMap =  {
+  "URL_NOT_SUPPORTED": "지원되지 않는 URL 입니다",
+  "URL_ALREADY_LISTED": "이미 등록되어있는 거래입니다"
+}
+
 export const extractErrorMessage = function(e) {
   const message = e.error_description || e.message || "";
   const match = message.match(/.+[A-Z_]+:(.+)/);
@@ -13,6 +19,6 @@ export const extractErrorMessage = function(e) {
 
 export const handleErrorMessage = e => {
   notification["error"]({
-    message: extractErrorMessage(e)
+    message: errorMap[extractErrorMessage(e)] || extractErrorMessage(e)
   });
 };
